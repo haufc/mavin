3f8c56d7b29372e66d159e55dfcfda6e1dae8e54
admin admin <evaladmin@example.com>
2020-03-25 09:58:24 +0000

    User admin wrote content /static-assets/js/search-bar.js

---
+++ static-assets/js/search-bar.js
@@ -0,0 +1,7 @@
+function openSearch() {
+    document.getElementById("search").style.display = "block";
+}
+
+function closeSearch() {
+    document.getElementById("search").style.display = "none";
+} 
