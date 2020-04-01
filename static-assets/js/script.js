d756db6de0186f6c4b0da7f7f01857a35da96378
admin admin <evaladmin@example.com>
2020-04-01 08:57:16 +0000

    User admin wrote content /static-assets/js/script.js

--- static-assets/js/script.js
+++ static-assets/js/script.js
@@ -1,7 +1,13 @@
-function myFunction(elementID) {
-    console.log('hautk');
-    var selector = '#' + elementID;
-    var element = $('#navLinks').find(selector);
-    console.log(element);
-    $(element).addClass('active');
-}
+// function myFunction(elementID) {
+//     console.log('hautk');
+//     var selector = '#' + elementID;
+//     var element = $('#navLinks').find(selector);
+//     console.log(element);
+//     $(element).addClass('active');
+// }
+$(document).ready(function() {
+    var url = window.location;
+    $('#navLinks a').filter(function() {
+        return this.href == url;
+    }).addClass('active');
+});
