f86be21d1401abe87f658f6a5ec5b22beb82f5aa
admin admin <evaladmin@example.com>
2020-04-03 04:17:32 +0000

    User admin wrote content /scripts/components/jobs.groovy

--- scripts/components/jobs.groovy
+++ scripts/components/jobs.groovy
@@ -1,11 +1,2 @@
-templateModel.varLue = "aaaaaaa"
 
-def queryStatement = 'content-type:"/page/home"'
-
-def query = searchService.createQuery()
-query.setQuery(queryStatement)
-
-def executedQuery = searchService.search(query)
-def itemsFound = executedQuery.response.numFound
-def items = executedQuery.response.documents
 print "this is imtem"
