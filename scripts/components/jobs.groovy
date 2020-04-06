f3f2cd3178c005aab867779356346a5bbbd47692
admin admin <evaladmin@example.com>
2020-04-06 07:14:39 +0000

    User admin wrote content /scripts/components/jobs.groovy

--- scripts/components/jobs.groovy
+++ scripts/components/jobs.groovy
@@ -17,7 +17,7 @@ def executedQuery = elasticsearch.search(new SearchRequest().source(builder))
 def itemsFound = executedQuery.hits.totalHits
 def items = executedQuery.hits.hits
 print "My list job 1"
-print items.image_s
+print items
 // items.each { item ->
 //             print item
 //   }
