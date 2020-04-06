1704c9a4d28fe4e19579e9b9b7b16ea464058bbb
admin admin <evaladmin@example.com>
2020-04-06 04:46:12 +0000

    User admin wrote content /scripts/components/jobs.groovy

--- scripts/components/jobs.groovy
+++ scripts/components/jobs.groovy
@@ -1,2 +1,20 @@
 
 print "this is imtem"
+// Import the required classes
+import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.builder.SearchSourceBuilder
+
+def queryStatement = 'content-type:"/component/article" AND author:"My User"'
+
+// Use the appropriate builders according to your query
+def builder = new SearchSourceBuilder()
+    .query(QueryBuilders.queryStringQuery(queryStatement))
+
+// Execute the query
+def executedQuery = elasticsearch.search(new SearchRequest().source(builder))
+
+def itemsFound = executedQuery.hits.totalHits
+def items = executedQuery.hits.hits
+
+// return items
