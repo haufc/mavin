ea0f835fee10de0707340bd1845f138b0c63ffd1
admin admin <evaladmin@example.com>
2020-04-09 08:59:47 +0000

    User admin wrote content /scripts/rest/search.get.groovy

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -3,7 +3,8 @@ import org.craftercms.blueprints.headless.SearchContentHelper
 println "This is param :"
 println params.q
 def searchHelper = new SearchContentHelper(elasticsearch, urlTransformationService)
-def jobs = searchHelper.searchHomes(params.q)
+
+def jobs = searchHelper.searchHomes(String.valueOf(params.q))
 
 println  jobs
 return "Hautk"
