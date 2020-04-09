e49cf407ce7f0ce8b5ab1aa570fb98c966922641
admin admin <evaladmin@example.com>
2020-04-09 06:20:09 +0000

    User admin wrote content /scripts/classes/org/craftercms/blueprints/headless/SearchContentHelper.groovy

---
+++ scripts/classes/org/craftercms/blueprints/headless/SearchContentHelper.groovy
@@ -0,0 +1,7 @@
+import org.apache.commons.lang3.StringUtils
+import org.craftercms.engine.service.UrlTransformationService
+import org.elasticsearch.action.search.SearchRequest
+import org.elasticsearch.index.query.QueryBuilders
+import org.elasticsearch.search.builder.SearchSourceBuilder
+import org.elasticsearch.search.sort.FieldSortBuilder
+import org.elasticsearch.search.sort.SortOrder
