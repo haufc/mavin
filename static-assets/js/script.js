c8b2c841a489c290f4c107861a94e83c9b3d1b43
admin admin <evaladmin@example.com>
2020-04-09 10:29:24 +0000

    User admin wrote content /static-assets/js/script.js

--- static-assets/js/script.js
+++ static-assets/js/script.js
@@ -6,7 +6,7 @@
 //     $(element).addClass('active');
 // }
 $(document).ready(function() {
-    $('search-result').css("display", "none");
+    $('.search-result').css("display", "none");
     var url = window.location;
     $('#navLinks a').filter(function() {
         return this.href == url;
@@ -25,7 +25,7 @@ function search() {
             success: function(resp){
                 console.log(resp);
                 let total = resp[0].length + resp[1].length;
-                $('search-result').css("display", "block");
+                $('.search-result').css("display", "block");
                 $('#total-search').text(total);
                 $('#term-search').text(userTerm);
             }
