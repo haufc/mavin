1e9638c61eb379ecd6053a4c10315da1f45894ce
admin admin <evaladmin@example.com>
2020-04-09 09:39:31 +0000

    User admin wrote content /scripts/rest/search.get.groovy

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -5,6 +5,7 @@ println params.q
 def searchHelper = new SearchContentHelper(elasticsearch, urlTransformationService)
 
 def jobs = searchHelper.searchJobs(params.q)
-
+def homes = searchHelper.searchHomes(params.q)
 println  jobs
+println homes
 return jobs
