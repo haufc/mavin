fd6c945b9852d4964302bce0a3ef2860a7bf0365
admin admin <evaladmin@example.com>
2020-04-10 09:56:04 +0000

    User admin wrote content /static-assets/js/script.js

--- static-assets/js/script.js
+++ static-assets/js/script.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 });
 
 function getContextPath() {
-   return window.location.pathname.substring(0, window.location.pathname.indexOf("/",2));
+   return  window.location.protocol + "//" + window.location.host
 }
 function search() {
         console.log('demo');
