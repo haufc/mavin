4ef71a8d599badfbde89a77c5d8b23d4624d7600
admin admin <evaladmin@example.com>
2020-04-13 03:45:03 +0000

    User admin wrote content /static-assets/js/script.js

--- static-assets/js/script.js
+++ static-assets/js/script.js
@@ -12,7 +12,20 @@ $(document).ready(function() {
     $('#navLinks a').filter(function() {
         return this.href == url;
     }).addClass('active');
+    
+    let dateVal = $('.date-formater').text();
+    $('.date-formater').text(this.formatDate(dateVal));
 });
+
+function formatDate(value) {
+    var date = new Date(value);
+    return (
+      (date.getMonth() > 8) 
+      ? (date.getMonth() + 1) 
+      : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) 
+        ? date.getDate() 
+        : ('0' + date.getDate())) + '/' + date.getFullYear();
+}
 
 function getContextPath() {
    return  window.location.protocol + "//" + window.location.host
