c9c2da80db32c57316e02067881f18edf80159a2
admin admin <evaladmin@example.com>
2020-04-15 03:35:04 +0000

    User admin wrote content /static-assets/js/script.js

--- static-assets/js/script.js
+++ static-assets/js/script.js
@@ -1,10 +1,3 @@
-// function myFunction(elementID) {
-//     console.log('hautk');
-//     var selector = '#' + elementID;
-//     var element = $('#navLinks').find(selector);
-//     console.log(element);
-//     $(element).addClass('active');
-// }
 $(document).ready(function() {
     $('.search-result').css("display", "none");
     var url = window.location;
@@ -90,31 +83,8 @@ function search() {
             type: "GET",
             url:urlService,
             success: function(resp){
-                console.log(resp);
-                //let total = resp[0].length + resp[1].length;
-               // let total = resp[0].length;
-               // $('.search-result').css("display", "block");
-               // $('#total-search').text(total);
-              //  $('#term-search').text(userTerm);
-                
-              //  var source = $("#search-results-template").html();
-             //   var template = Handlebars.compile(source);
-             //   var context = { results: resp[0] };
-                localStorage.setItem("listSearch", JSON.stringify([0]));
+                localStorage.setItem("listSearch", JSON.stringify(resp[0]));
                 window.location.replace(getContextPath()+ "/search-result");
-               // var html = template(context);
-                
-             //   $('.search-result__list').html(html);
-                
-                // --- limit desc search --
-              //  var lent = $(".limit-text-250").html().length;
-               // if (lent > 200) {
-                //  short_text = $(".limit-text-250").html().substr(0, 200);
-               //   $(".limit-text-250").html(short_text + "...");
-              //  }
-                
-                // --- style for em--
-               // $(".limit-text-250 em").css("font-weight","bold");
             }
         });
     $('.nav-bar__search').css("display", "none");
