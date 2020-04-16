c5830964cb74dd380a024308433cefd6d3629abf
admin admin <evaladmin@example.com>
2020-04-16 08:14:28 +0000

    User admin wrote content /scripts/components/introductcontent.groovy

--- scripts/components/introductcontent.groovy
+++ scripts/components/introductcontent.groovy
@@ -1,1 +1,8 @@
+import org.craftercms.blueprints.headless.IntroductSearchHelper
+
+
+def searchHelper = new IntroductSearchHelper(elasticsearch, urlTransformationService)
+def intros = searchHelper.searchIntroducts(null)
 print "hello every body"
+
+print intros
