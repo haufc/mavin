1628f90f71beb656895f50ab8370dd7b21903c97
admin admin <evaladmin@example.com>
2020-04-23 09:07:01 +0000

    User admin wrote content /static-assets/js/script.js

--- static-assets/js/script.js
+++ static-assets/js/script.js
@@ -2,9 +2,13 @@ $(document).ready(function() {
     $('.search-result').css("display", "none");
     var url = window.location;
     
+    // $('#navLinks a').filter(function() {
+    //     return this.href == url;
+    // }).addClass('active');
+    
     $('#navLinks a').filter(function() {
         return this.href == url;
-    }).addClass('active');
+    }).css('font-weight', 'bold');
     
     let dateVal = $('.date-formater').text();
     $('.date-formater').text(formatDate(dateVal));
