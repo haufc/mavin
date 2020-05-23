25d587373bdb180a008805d576395fed56be9bb8
admin admin <evaladmin@example.com>
2020-05-23 07:58:30 +0000

    User admin wrote content /scripts/components/productchildgroup.groovy

--- scripts/components/productchildgroup.groovy
+++ scripts/components/productchildgroup.groovy
@@ -1,6 +1,7 @@
 import org.craftercms.blueprints.headless.GroupProductSearchHelper
 
 def productGroup = contentModel.productgrouplevel2_s
+print productGroup
 def searchHelper = new GroupProductSearchHelper()
-def products = searchHelper.searchProducts(productGroup)
+def products = searchHelper.searchProducts(productGroup, 0)
 print products
