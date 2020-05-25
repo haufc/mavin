147fb1b0c6718ed7b087f66098057e09a6337b13
admin admin <evaladmin@example.com>
2020-05-25 03:56:44 +0000

    User admin wrote content /scripts/pages/productdetail.groovy

--- scripts/pages/productdetail.groovy
+++ scripts/pages/productdetail.groovy
@@ -1,5 +1,5 @@
 import org.craftercms.blueprints.headless.GroupProductSearchHelper
 
-def productGroup = contentModel.productchildgroup_o.item[0].key
+def productGroup = contentModel.productchildgroup_o.item.key
 print "this - is key: "
-print productGroup
+print productGroup.text
