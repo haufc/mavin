7acc9ed94dd4c8936f595bb2c885ed5a9ca167a2
admin admin <evaladmin@example.com>
2020-05-26 15:08:46 +0700

    Delete file /scripts/components/news.groovy

--- scripts/components/news.groovy
+++
@@ -1,9 +0,0 @@
-import org.craftercms.blueprints.headless.NewsSearchHelper
-
-
-def searchHelper = new NewsSearchHelper(elasticsearch, urlTransformationService)
-def news = searchHelper.search(null)
-println "List News:"
-print news
-
-templateModel.news = news
