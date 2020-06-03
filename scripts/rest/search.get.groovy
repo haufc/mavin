1b00db5840f1d15b9974054edd0ec9bdee3adb51
admin admin <evaladmin@example.com>
2020-06-03 08:28:36 +0000

    User admin wrote content /scripts/rest/search.get.groovy

--- scripts/rest/search.get.groovy
+++ scripts/rest/search.get.groovy
@@ -10,17 +10,20 @@ def intros = searchHelper.searchIntros(params.q)
 def news = searchHelper.searchNews(params.q)
 def products = searchHelper.searchProducts(params.q)
 
-println "This is Jobs : =================================>"
-println  jobs
+// println "This is Jobs : =================================>"
+// println  jobs
 
-println "This is Intros : =================================>"
-println intros
+// println "This is Intros : =================================>"
+// println intros
 
-println "This is News : =================================>"
-println news
+// println "This is News : =================================>"
+// println news
+ news.each{ new1 ->
+     This is News : =================================>
+     println new1.highlight
+ }
 
-
-println "This is product : =================================>"
-println products
+// println "This is product : =================================>"
+// println products
 
 return [jobs, intros, news, products]
