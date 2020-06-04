9a6f3c896db603d2a62f3a4a47ce8754bea702af
admin admin <evaladmin@example.com>
2020-06-04 07:10:12 +0000

    User admin wrote content /scripts/pages/productdetail.groovy

--- scripts/pages/productdetail.groovy
+++ scripts/pages/productdetail.groovy
@@ -4,6 +4,6 @@ def productGroup = contentModel.productchildgroup_o.item.key
 def searchHelper = new GroupProductSearchHelper(elasticsearch, urlTransformationService)
 def relatedProducts = searchHelper.searchProducts(productGroup.text, 0,5)
 print "==================>"
-print contentModel.productchildgroup_o.item.each{item -> print item.value}
+
 print relatedProducts
 templateModel.relatedProducts = relatedProducts
