e3d8dd35f1bc269c1bfb09aaf1a15f18375a4624
admin admin <evaladmin@example.com>
2020-06-05 09:46:27 +0000

    User admin wrote content /static-assets/js/language_selector.js

--- static-assets/js/language_selector.js
+++ static-assets/js/language_selector.js
@@ -1,14 +1,3 @@
 $(document).ready(function() {
     $('#languageSelector').selectpicker();
-    var url = window.location;
-   console.log(url);
-   var li = $('.dropdown-menu').find('ul').find('li');
-    if (url.href.indexOf('/en') > -1) {
-     
-     $(li[1]).removeClass('active');           
-        $(li[0]).addClass('active');
-    } else {
-        $(li[0]).removeClass('active');           
-        $(li[1]).addClass('active');
-    }
 });
