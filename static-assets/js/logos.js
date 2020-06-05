00c06609c960a4b1baa952c624c000a6ae23e128
admin admin <evaladmin@example.com>
2020-06-05 14:59:24 +0700

    User admin wrote content /static-assets/js/logos.js

--- static-assets/js/logos.js
+++ static-assets/js/logos.js
@@ -7,15 +7,13 @@ owl.owlCarousel({
     autoplay: true,
     autoplayTimeout: 1000,
     autoplayHoverPause: true,
-    // responsiveClass: true,
-    // responsive: {
-    //     600: {
-    //         items: 4,
-    //         nav: true
-    //     },
-    //     300: {
-    //         items: 2,
-    //         nav: true
-    //     }
-    // }
+    responsiveClass: true,
+    responsive: {
+        600: {
+            items: 4
+        },
+        300: {
+            items: 4
+        }
+    }
 });
