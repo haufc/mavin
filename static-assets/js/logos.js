5aefdc3da2740d6c6d0c4d11a0f5be16756f6a5c
admin admin <evaladmin@example.com>
2020-06-05 14:38:14 +0700

    User admin wrote content /static-assets/js/logos.js

--- static-assets/js/logos.js
+++ static-assets/js/logos.js
@@ -1,7 +1,26 @@
 var owl = $('.owl-carousel');
 owl.owlCarousel({
-    items: 8,
-    loop: true,
+    responsive:{
+        0:{
+            items:1,
+            nav:true,
+            loop: true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        },
+        1201:{
+            items:8,
+            nav:true,
+            loop:false
+        }
+    },
     margin: 15,
     dots: false,
     autoplay: true,
