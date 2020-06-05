13d6b4fbcc319828a21f7c588546dd11faab2d3b
admin admin <evaladmin@example.com>
2020-06-05 09:45:02 +0000

    User admin wrote content /static-assets/js/language_selector.js

--- static-assets/js/language_selector.js
+++ static-assets/js/language_selector.js
@@ -1,12 +1,14 @@
 $(document).ready(function() {
     $('#languageSelector').selectpicker();
-     var language = $('#languageSelector').val();  
-     var li = $('.dropdown-menu').find('ul').find('li');  
-     if (language == 'VIE') { 
-         $(li[1]).removeClass('active');           
-         $(li[0]).addClass('active');
-     } else {
-         $(li[0]).removeClass('active');           
-         $(li[1]).addClass('active');
-     }
+    var url = window.location;
+   console.log(url);
+   var li = $('.dropdown-menu').find('ul').find('li');
+    if (url.href.indexOf('/en') > -1) {
+     
+     $(li[1]).removeClass('active');           
+        $(li[0]).addClass('active');
+    } else {
+        $(li[0]).removeClass('active');           
+        $(li[1]).addClass('active');
+    }
 });
