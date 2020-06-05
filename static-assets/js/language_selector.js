6ec9a364688ad614eeec349530d5225dc1a08baf
admin admin <evaladmin@example.com>
2020-06-05 09:40:44 +0000

    User admin wrote content /static-assets/js/language_selector.js

--- static-assets/js/language_selector.js
+++ static-assets/js/language_selector.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
     $('#languageSelector').selectpicker();
+     var language = $('#languageSelector').val();  
+     var li = $('.dropdown-menu').find('ul').find('li');  
+     if (language == 'VIE') { 
+         $(li[1]).removeClass('active');           
+         $(li[0]).addClass('active');
+     } else {
+         $(li[0]).removeClass('active');           
+         $(li[1]).addClass('active');
+     }
 });
