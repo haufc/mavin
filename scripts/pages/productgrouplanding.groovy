0aa79812220383ed5a6d8ea4c2294f0cd35781c3
admin admin <evaladmin@example.com>
2020-06-09 03:38:37 +0000

    User admin wrote content /scripts/pages/productgrouplanding.groovy

--- scripts/pages/productgrouplanding.groovy
+++ scripts/pages/productgrouplanding.groovy
@@ -1,1 +1,6 @@
-print "hello i am here"
+import org.craftercms.blueprints.headless.TaxonomySearchHelper
+
+def categories = new TaxonomySearchHelper("product-categories", elasticsearch, siteItemService)
+						.getItems()
+						
+templateModel.groupProduct = categories
