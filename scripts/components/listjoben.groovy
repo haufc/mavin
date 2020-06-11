1cd7342f073449d52a0f3f475022cb1bc4f2dc0f
admin admin <evaladmin@example.com>
2020-06-11 04:27:08 +0000

    User admin wrote content /scripts/components/listjoben.groovy

--- scripts/components/listjoben.groovy
+++ scripts/components/listjoben.groovy
@@ -1,6 +1,6 @@
 import org.craftercms.blueprints.headless.JobEnSearchHelper
 
 
-def searchHelper = new JobSearchHelper(elasticsearch, urlTransformationService)
+def searchHelper = new JobEnSearchHelper(elasticsearch, urlTransformationService)
 def jobs = searchHelper.searchJobs(null,0,6)
 templateModel.jobs = jobs
