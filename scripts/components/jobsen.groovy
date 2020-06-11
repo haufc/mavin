b9589e98877dbf088bad61a21ba94e50c905904a
admin admin <evaladmin@example.com>
2020-06-11 03:28:09 +0000

    Delete file /scripts/components/jobsen.groovy

--- scripts/components/jobsen.groovy
+++
@@ -1,6 +0,0 @@
-import org.craftercms.blueprints.headless.JobSearchHelper
-
-
-def searchHelper = new JobSearchHelper(elasticsearch, urlTransformationService)
-def jobs = searchHelper.searchJobs(null)
-templateModel.jobs = jobs
