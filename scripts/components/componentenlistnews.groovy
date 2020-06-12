7de2ac9251865e5ac29765f99e53b73881a174d5
admin admin <evaladmin@example.com>
2020-06-12 15:23:27 +0700

    User admin wrote content /scripts/components/componentenlistnews.groovy

--- scripts/components/componentenlistnews.groovy
+++ scripts/components/componentenlistnews.groovy
@@ -2,7 +2,7 @@ import org.craftercms.blueprints.headless.NewsEnSearchHelper
 
 
 def searchHelper = new NewsEnSearchHelper(elasticsearch, urlTransformationService)
-def news = searchHelper.searchNews(null)
+def news = searchHelper.searchEnNews(null)
 println "News:"
 print news
 
