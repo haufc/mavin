ecc7cbcb347b3c4a4965ed647391c6f654959340
admin admin <evaladmin@example.com>
2020-06-15 19:31:58 +0700

    User admin wrote content /scripts/pages/productdetail.groovy

--- scripts/pages/productdetail.groovy
+++ scripts/pages/productdetail.groovy
@@ -3,7 +3,7 @@ import org.craftercms.blueprints.headless.TaxonomySearchHelper
 
 def productGroup = contentModel.productchildgroup_o.item.key
 def searchHelper = new GroupProductSearchHelper(elasticsearch, urlTransformationService)
-def relatedProducts = searchHelper.searchProducts(productGroup.text, 0,5)
+def relatedProducts = searchHelper.searchProducts(productGroup.text, 0,4)
 
 def productVN = relatedProducts.findAll { it.url.indexOf('/en') < 0 }
 def productEN = relatedProducts.findAll { it.url.indexOf('/en') > -1}
