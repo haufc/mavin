db8e37ae18c0f05d8167e273f64b92d9499e4dd4
admin admin <evaladmin@example.com>
2020-06-15 20:25:53 +0700

    User admin wrote content /scripts/pages/productdetail.groovy

--- scripts/pages/productdetail.groovy
+++ scripts/pages/productdetail.groovy
@@ -1,6 +1,5 @@
 import org.craftercms.blueprints.headless.GroupProductSearchHelper
 import org.craftercms.blueprints.headless.TaxonomySearchHelper
-import java.util.Arrays
 
 def productGroup = contentModel.productchildgroup_o.item.key
 def searchHelper = new GroupProductSearchHelper(elasticsearch, urlTransformationService)
@@ -9,13 +8,6 @@ def relatedProducts = searchHelper.searchProducts(productGroup.text, 0)
 def productVN = relatedProducts.findAll { it.url.indexOf('/en') < 0 }
 def productEN = relatedProducts.findAll { it.url.indexOf('/en') > -1}
 
-if (productVN.size() > 4) {
-    productVN = Arrays.copyOf(productVN, 4);
-}
-
-if (productEN.size() > 4) {
-    productEN = Arrays.copyOf(productEN, 4);
-}
 
 def categories = new TaxonomySearchHelper("product-category-child", elasticsearch, siteItemService)
 						.getItems()
